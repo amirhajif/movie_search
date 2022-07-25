505670862c720b55fd2b6a5e6ffa16e04d6ef1f1
amirhajif <amirhajitabar2@gmail.com>
2022-07-25 21:00:46 +0430

    Complete search keyup event to show movies list in search based on what text written in search box

--- app.js
+++ app.js
@@ -8,7 +8,24 @@ async function loadMovies(searchTerm){
     const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
-    console.log(data)
+    //check the response be ok 
+    if(data.Response== "True"){
+        //all data about videos exist in Search of data
+        displayMovieList(data.Search)
+    }
 }
 
-loadMovies('lord of the rings')
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim()
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list')
+        loadMovies(searchTerm)
+    }
+    else{
+        searchList.classList.add('hide-search-list')
+    }
+}
+
+function displayMovieList(movies){
+
+}
