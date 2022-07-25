ed6863997adb1c99908f3df332fad1a2539d5e12
amirhajif <amirhajitabar2@gmail.com>
2022-07-25 21:32:03 +0430

    show all videos searched in search box with poster and title and year but unclickable

--- app.js
+++ app.js
@@ -15,17 +15,56 @@ async function loadMovies(searchTerm){
     }
 }
 
+//keyup event for show suggested movie based on text written
 function findMovies(){
+    //save text with trim 
     let searchTerm = (movieSearchBox.value).trim()
+    //when text written
     if(searchTerm.length > 0){
+        //delete display none 
         searchList.classList.remove('hide-search-list')
+        //get movies from api and display them
         loadMovies(searchTerm)
     }
     else{
+        //add display none 
         searchList.classList.add('hide-search-list')
     }
 }
 
+//for showing on time searched movie base on written text on search box
 function displayMovieList(movies){
+    //complete search list based on movies get from api
+    searchList.innerHTML=""
+    movies.forEach(movie => {
+        //create a division for build videos deatails
+        let movieListItem = document.createElement('div')
+        //dataset of video add from imdbID in api
+        movieListItem.dataset.id=movie.imdbID;
+        //add class list
+        movieListItem.classList.add('search-list-item')
+        //movie poster on api has 2 state:1-N/A without poster
+        //2-has poster and load from api
+        if(movie.Poster!=="N/A"){
+            //load poster from Poster in api response
+            moviePoster=movie.Poster
+        }
+        else{
+            //if image dosent set load not found image 
+            moviePoster='../images/image_not_found.png'
+        }
+        //set movie poster and Title and Year of video from api result
+        movieListItem.innerHTML=`
+              <div class="search-item-thumbnail">
+                <img src="${moviePoster}" />
+              </div>
+              <div class="search-item-info">
+                <h3>${movie.Title}</h3>
+                <p>${movie.Year}</p>
+              </div>
+        `
+        // add completed division into parent element for show on search 
+        searchList.appendChild(movieListItem)
+    });
 
 }
