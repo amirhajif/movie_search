1509533384f1b31315b5be687118a8f9c9ce7bbf
amirhajif <amirhajitabar2@gmail.com>
2022-07-27 13:57:09 +0430

    display movies all details in page

--- app.js
+++ app.js
@@ -70,22 +70,45 @@ function displayMovieList(movies){
     //load movie after searching and press enter
     loadMovieDetails()
 }
+    //for get movie all data from omdbapi 
     function loadMovieDetails(){
+        //collect all items show in search list bae on text in searchListMovies array
         const searchListMovies = searchList.querySelectorAll('.search-list-item')
+        //add click event to all items show in search list
         searchListMovies.forEach(movie => {
             movie.addEventListener('click',async()=>{
-                // console.log(movie.dataset.id)
+                //hide all suggested movie in search list
                 searchList.classList.add('hide-search-list')
+                //empty textbox for search movie
                 movieSearchBox.value=""
+                //get result
                 const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=9ed8b13b`)
-                // console.log(result)
                 const movieDetail = await result.json()
-                console.log(movieDetail)
+                //call displayeMovieDetails for show movie all detail in page
                 displayeMovieDetails(movieDetail)
             })
 
         });
     }
+    //for show movie details completely in page
     function displayeMovieDetails(movieDetail){
-         
+         resultGrid.innerHTML=`
+            <div class="movie-poster">
+              <img src="${(movieDetail.Poster!="N/A")?movieDetail.Poster:"./images/image_not_found.png"}"  alt="movie poster" />
+            </div>
+            <div class="movie-info">
+              <h3 class="movie-title">${movieDetail.Title}</h3>
+              <ul class="movie-misc-info">
+                <li class="year">Year: ${movieDetail.Year}</li>
+                <li class="rated">Ratings: ${movieDetail.Rated}</li>
+                <li class="released">Released: ${movieDetail.Released}</li>
+              </ul>
+              <p class = "genre"><b>Genre:</b> ${movieDetail.Genre}</p>
+              <p class = "writer"><b>Writer:</b> ${movieDetail.Writer}</p>
+              <p class = "actors"><b>Actors: </b>${movieDetail.Actors}</p>
+              <p class = "plot"><b>Plot:</b> ${movieDetail.Plot}</p>
+              <p class = "language"><b>Language:</b> ${movieDetail.Language}</p>
+              <p class = "awards"><b><i class = "fas fa-award"></i></b> ${movieDetail.Awards}</p>
+            </div>
+         `
     }
